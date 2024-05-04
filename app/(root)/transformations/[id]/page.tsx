ebac27dd9f1147863f1a1460783afc6e2a8218f3
Manny <contact@manny-teto.com>
2024-05-04 23:47:15 +0700

    initial commit

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,5 @@
+const TransformationsPage = () => {
+  return <div>Transformations</div>;
+};
+
+export default TransformationsPage;
---
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -0,0 +1,5 @@
+const UpdateTransformationPage = () => {
+  return <div>Update Transformation</div>;
+};
+
+export default UpdateTransformationPage;
